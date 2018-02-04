9244f3b430946ce1d3ea6959c256ed8385076ab2
sandyfox1993 <asd@asd.we>
2018-02-04 23:44:51 +0300

    Walls added

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,14 +10,8 @@ namespace Snake
 
             //Console.SetBufferSize(80,25);
 
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-			VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
-            VerticalLine rightLine = new VerticalLine(0, 24,78, '+');
-            upLine.Draw();
-            downLine.Draw();
-            leftLine.Draw();
-            rightLine.Draw();
+            Walls walls = new Walls(78, 24);
+            walls.Draw();
 
             FoodCreator fc = new FoodCreator(78, 24, '$');
             Point newFood = fc.CreateFood();
@@ -27,6 +21,10 @@ namespace Snake
             Snake snake = new Snake(p,4,Direction.RIGHT);
             snake.Draw();
             while (true){
+                if (walls.isHit(snake)||snake.isHitTail()){
+                    break;
+                }
+
                 if (snake.eatFood(newFood)){
                     newFood = fc.CreateFood();
                     newFood.Draw();
